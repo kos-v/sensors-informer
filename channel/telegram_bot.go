69b1406931ca12b6965d9672db789825d8af56cd
Konstantin Voloshchuk <kosv.dev@gmail.com>
2021-11-29 17:43:22 +0600

    Hide secrets in errors and logs

--- channel/telegram_bot.go
+++ channel/telegram_bot.go
@@ -5,6 +5,8 @@ import (
 	botapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/kos-v/sensors-informer/config"
 	"github.com/kos-v/sensors-informer/report"
+	"strings"
+	"unicode/utf8"
 )
 
 type TelegramBot struct {
@@ -18,13 +20,13 @@ func (ch *TelegramBot) IsEnable() bool {
 func (ch *TelegramBot) Send(r report.Report) error {
 	bot, err := botapi.NewBotAPI(ch.Config.Channels.TelegramBot.Token)
 	if err != nil {
-		return err
+		return ch.hideSecrets(err)
 	}
 
 	msg := botapi.NewMessage(ch.Config.Channels.TelegramBot.ChatId, ch.format(r))
 	_, err = bot.Send(msg)
 
-	return err
+	return ch.hideSecrets(err)
 }
 
 func (ch *TelegramBot) format(r report.Report) string {
@@ -33,4 +35,13 @@ func (ch *TelegramBot) format(r report.Report) string {
 		msg += fmt.Sprintf("\"%s::%s\": %.1f°С\n", v.BusName, v.SensorName, v.SensorValue)
 	}
 	return msg
+}
+
+func (ch *TelegramBot) hideSecrets(err error) error {
+	return fmt.Errorf(strings.Replace(
+		err.Error(),
+		ch.Config.Channels.TelegramBot.Token,
+		strings.Repeat("*", utf8.RuneCountInString(ch.Config.Channels.TelegramBot.Token)),
+		-1,
+	))
 }
