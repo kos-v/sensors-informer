8c2dc9a99271882ad57b931d2808b9c1a00853b3
Konstantin Voloshchuk <kosv.dev@gmail.com>
2021-12-03 15:31:58 +0600

    Rename var

--- cmd/informer/main.go
+++ cmd/informer/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/kos-v/sensors-informer/internal"
 	"github.com/kos-v/sensors-informer/internal/channel"
-	"github.com/kos-v/sensors-informer/internal/config"
+	conf "github.com/kos-v/sensors-informer/internal/config"
 	"github.com/kos-v/sensors-informer/internal/message"
 	"github.com/kos-v/sensors-informer/internal/report"
 	"github.com/kos-v/sensors-informer/internal/sensor"
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	conf, err := config.LoadConfig()
+	config, err := conf.LoadConfig()
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
@@ -20,14 +20,14 @@ func main() {
 
 	listener := internal.ReportListener{
 		Ch:       rch,
-		Channels: channel.GetChannels(*conf, &message.PredictableFormatter{}),
-		Config:   *conf,
+		Channels: channel.GetChannels(*config, &message.PredictableFormatter{}),
+		Config:   *config,
 	}
 	listener.Listen()
 
 	detector := internal.Detector{
-		Config: *conf,
-		Reader: &sensor.CommandReader{conf.LmSensors.Command},
+		Config: *config,
+		Reader: &sensor.CommandReader{config.LmSensors.Command},
 		Rch:    rch,
 	}
 	detector.Run()
