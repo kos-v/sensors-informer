1258cce6a24ef6b4b92caa4da6281662016cdf08
Konstantin Voloshchuk <kosv.dev@gmail.com>
2021-12-30 12:01:02 +0600

    Refact: move common structs to sensor file

--- internal/sensor/parser.go
+++ internal/sensor/parser.go
@@ -2,7 +2,6 @@ package sensor
 
 import (
 	"regexp"
-	"strconv"
 	"strings"
 )
 
@@ -11,46 +10,6 @@ const (
 	sensorInfoPattern = `^  (\w*)(:\s*)(\d+\.\d+)$`
 	sensorNamePattern = `^([^ ].+):$`
 )
-
-type Bus struct {
-	Name    string
-	Sensors []Sensor
-}
-
-type Sensor struct {
-	Name string
-	Info []SensorInfo
-	Type string
-}
-
-func (s Sensor) HasInputInfo() bool {
-	return s.GetInputInfo().Name != ""
-}
-
-func (s Sensor) GetInputInfo() SensorInfo {
-	for _, v := range s.Info {
-		if v.Name == "input" {
-			return v
-		}
-	}
-
-	return SensorInfo{}
-}
-
-type SensorInfo struct {
-	FullName string
-	Name     string
-	Value    string
-}
-
-func (i SensorInfo) GetValueAsFloat() float32 {
-	f, err := strconv.ParseFloat(i.Value, 32)
-	if err != nil {
-		return 0
-	}
-
-	return float32(f)
-}
 
 type Parser struct {
 	SensorsOut string
--- internal/sensor/sensor.go
+++ internal/sensor/sensor.go
@@ -1,7 +1,49 @@
 package sensor
 
+import "strconv"
+
 const (
 	SensorTypeFan         = "fan"
 	SensorTypeTemperature = "temperature"
 	SensorTypeUnknown     = "unknown"
 )
+
+type Bus struct {
+	Name    string
+	Sensors []Sensor
+}
+
+type Sensor struct {
+	Name string
+	Info []SensorInfo
+	Type string
+}
+
+func (s Sensor) HasInputInfo() bool {
+	return s.GetInputInfo().Name != ""
+}
+
+func (s Sensor) GetInputInfo() SensorInfo {
+	for _, v := range s.Info {
+		if v.Name == "input" {
+			return v
+		}
+	}
+
+	return SensorInfo{}
+}
+
+type SensorInfo struct {
+	FullName string
+	Name     string
+	Value    string
+}
+
+func (i SensorInfo) GetValueAsFloat() float32 {
+	f, err := strconv.ParseFloat(i.Value, 32)
+	if err != nil {
+		return 0
+	}
+
+	return float32(f)
+}
